c64ca6b701954b3792e45a485dd3d4fb27cf9923
Lin Zihan <themagicnight@163.com>
2021-04-22 09:57:05 -0700

    wrapper doing

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,10 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    WrappingInt32 seqno = WrappingInt32{isn};
+    uint32_t offset = n % (uint64_t(UINT32_MAX) + 1);
+    seqno = seqno + offset;
+    return seqno;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,10 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t abs_seqno;
+    abs_seqno = n - isn;
+    while(checkpoint > abs_seqno){
+        abs_seqno = abs_seqno + (uint64_t(UINT32_MAX) + 1);
+    }
+    return abs_seqno;
 }
