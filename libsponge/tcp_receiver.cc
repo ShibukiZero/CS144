77f763cfcf9e773bd8fb390d709679b25ae99ad6
Lin Zihan <themagicnight@163.com>
2021-04-23 09:10:39 -0700

    tcp receiver segment reveived doing

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,21 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
+    // receive the TCP segment header before further processing.
     TCPHeader header = seg.header();
-    if (header.syn == true && _connected == false) {
-        _connected = true;
+    // if it's a syn segment, set connection and record isn.
+    if (header.syn == 1){
+        _connected = 1;
+        _isn = header.seqno;
+    }
+    // if connection is built,
+    if (_connected){
+        bool eof = header.fin;
+        uint64_t abs_seqno = unwrap(header.seqno, _isn, _first_unassembled);
+    }
+    // if there is no connection, do nothing.
+    else{
+        return;
     }
 }
 
