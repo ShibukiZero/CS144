9461a9ae9a8e3698d867a9fa89195c0a75dbbe23
Lin Zihan <themagicnight@163.com>
2021-04-25 02:52:29 -0700

    retransmission timer doing

--- libsponge/retransmission_timer.cc
+++ libsponge/retransmission_timer.cc
@@ -1,2 +1,12 @@
 #include "retransmission_timer.hh"
 
+RetransmissionTimer::RetransmissionTimer(const uint16_t initial_retransmission_timeout)
+    : _initial_retransmission_timeout(initial_retransmission_timeout)
+    , _retransmission_timeout(initial_retransmission_timeout)
+    , _timer(0){};
+
+void RetransmissionTimer::start() {
+    _timer = 0;
+}
+
+bool RetransmissionTimer::alart(const std::size_t ms_since_last_tick) {}
--- libsponge/retransmission_timer.hh
+++ libsponge/retransmission_timer.hh
@@ -1,5 +1,6 @@
 #ifndef SPONGE_RETRANSMISSION_TIMER_HH
 #define SPONGE_RETRANSMISSION_TIMER_HH
+#include <cstdint>
 
 //! \brief The timer which goes off if transmission times out.
 
@@ -9,7 +10,18 @@ class RetransmissionTimer{
   private:
     unsigned int _initial_retransmission_timeout;
     unsigned int _retransmission_timeout;
+    unsigned int _timer;
 
+  public:
+    RetransmissionTimer(const uint16_t initial_retransmission_timeout);
+
+    void start();
+
+    bool alart(const size_t ms_since_last_tick);
+
+    void double_RTO();
+
+    void reset_RTO();
 };
 
 #endif //SPONGE_RETRANSMISSION_TIMER_HH
