73671e82b0848b4f29d3a632b1d28a2b203e54f5
Lin Zihan <themagicnight@163.com>
2021-04-30 04:18:39 -0700

    reassembler optimizing

---
+++ libsponge/reassembler_buffer.cc
@@ -0,0 +1,1 @@
+#include "reassembler_buffer.hh"
---
+++ libsponge/reassembler_buffer.hh
@@ -0,0 +1,9 @@
+#ifndef SPONGE_REASSEMBLER_BUFFER_HH
+#define SPONGE_REASSEMBLER_BUFFER_HH
+#include <string>
+#include <map>
+
+struct Substring{
+
+};
+#endif //SPONGE_REASSEMBLER_BUFFER_HH
