b738988359f4bb766c399f568579a8efe15e6376
Lin Zihan <themagicnight@163.com>
2021-05-04 20:18:19 -0700

    CS144 Lab 4 stream reassembler code review finished

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,6 +1,5 @@
 #include "stream_reassembler.hh"
 #include "reassembler_buffer.hh"
-#include <iostream>
 
 // Dummy implementation of a stream reassembler.
 
@@ -21,71 +20,56 @@ StreamReassembler::StreamReassembler(const size_t capacity)
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const uint64_t index, const bool eof) {
-    // if stream has already been reassembled, do nothing.
     if (_output.input_ended()) {
         return;
     }
-    // if the substring is the last piece of stream, record the index of it.
     if (eof) {
         _eof_index = index + data.length();
-        // special case, where tracker is already at eof index.
-        // usually, this situation occurs when substring is an empty string.
+        // Note: this scene usually occurs when a substring with eof flag which
+        // has no data has been received.
         if (_eof_index == _tracker) {
             _output.end_input();
             return;
         }
     }
-    // if there are some data that have already written into byte stream, reassembler drops those data.
+    // If there are some data that have already written into byte stream, reassembler drops those data.
     const uint64_t start_index = max(_tracker, index);
-    // if there are some data that beyond capacity, reassembler drops those data.
+
+    // If there are some data that exceed capacity, reassembler drops those data.
     const uint64_t end_index = min(index + data.length(), _tracker + _output.remaining_capacity());
-    // if end_index is less or equal to start_index, either substring is already written into
-    // byte stream or the substring is beyond capacity. In both case, do nothing.
+
+    // If end_index is less or equal to start_index, either substring is already written into
+    // byte stream or the substring exceeds capacity. In both case, do nothing.
     if (end_index <= start_index) {
         return;
-    }
-    // if substring contains a continuous chunk of stream from tracker, write them into byte stream.
-    else if (start_index == _tracker) {
-        // write data into byte stream
-        if (start_index >= index){
-            _output.write(data.substr(start_index - index, end_index - start_index));
-        } else {
-            cerr << "Error position 1";
-        }
-//        _output.write(data.substr(start_index - index, end_index - start_index));
-//        //! \note perhaps bug happens here
-        // update the tracker
+    } else if (start_index == _tracker) {
+        // If substring contains a continuous chunk of stream from tracker, write them into byte stream.
+        _output.write(data.substr(start_index - index, end_index - start_index));
         _tracker = end_index;
-        // for any data that have already written, delete them in buffer.
         while (_unassembled.front().has_value()){
-            if (_unassembled.front()->start_index > _tracker + 1){
+            if (_unassembled.front()->start_index > _tracker) {
+                // Bytes stored in buffer are not continuous.
                 break;
-            }
-            else if (_unassembled.front()->end_index < _tracker){
+            } else if (_unassembled.front()->end_index <= _tracker) {
+                // Bytes stored in buffer already in stream.
                 _unassembled.pop();
                 continue;
-            }
-            else{
+            } else {
+                // Write the continuous part of data into stream.
                 Substring substring = _unassembled.front().value();
-                if (_tracker >= substring.start_index){
-                    _output.write(substring.data_string.substr(_tracker - substring.start_index));
-                } else {
-                    cerr << "Error position 2";
-                }
-//                _output.write(substring.data_string.substr(_tracker - substring.start_index));
-                //! \note perhaps bug happens here
+                _output.write(substring.data_string.substr(_tracker - substring.start_index));
                 _tracker = _output.bytes_written();
                 _unassembled.pop();
                 break;
             }
         }
-        // if tracker is beyond eof index, it means all data have been received. Set byte stream end_input.
+        // If tracker exceeds eof index, it means all data have been received. Set byte stream end_input.
         if (_tracker == _eof_index) {
             _output.end_input();
         }
         return;
     }
-    // if substring is ahead of current tracker, store them in buffer.
+    // If substring is ahead of current tracker, store them in buffer.
     else {
         _unassembled.push(Substring(index, data));
         return;
